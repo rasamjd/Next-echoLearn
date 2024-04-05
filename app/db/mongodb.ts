fef8947856c0d6cd667a04a6a927f960ae72faba
rasamjd <89083599+rasamjd@users.noreply.github.com>
2024-04-05 23:55:08 +0330

    Update mongodb.ts

--- app/db/mongodb.ts
+++ app/db/mongodb.ts
@@ -1,8 +1,5 @@
 import mongoose from "mongoose";
 
-const uri = "mongodb+srv://Rasa:rasarasadbaccess@cluster0.yo8wl.mongodb.net/echoLearnDatabase?retryWrites=true&w=majority"
-const uri2 = "mongodb+srv://nextRasa:next-echoLearn@cluster0.yo8wl.mongodb.net/?retryWrites=true&w=majority"
-
 export default async function databaseConncection() {
   try {
     await mongoose.connect(String(process.env.MONGODB_URI))
